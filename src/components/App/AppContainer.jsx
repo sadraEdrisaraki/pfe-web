f2a666c4700f0feb214319d751a8dcc8c76ac269
sadra edrisaraki <sadraedrisaraki@gmail.com>
2020-12-01 13:11:11 +0100

    add env config test

--- src/components/App/AppContainer.jsx
+++ src/components/App/AppContainer.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import * as config from "./../../config/appsettings.json"
-
 import { ProviderWrapper as QRCodeProvider } from "../../contexts/QRCodeContext";
-
 import App from "./App";
-console.log(config.test)
 
 const AppContainer = () => {
 	return (
