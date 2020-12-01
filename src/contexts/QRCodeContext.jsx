d1ad2d2ad09f1cc8e7f97ee5cbf14a47b46b00f9
Adrien <adrien_lemoine@hotmail.com>
2020-12-01 15:20:04 +0100

    QRCodeContext

--- src/contexts/QRCodeContext.jsx
+++ src/contexts/QRCodeContext.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 
+import participantService from "services/ParticipantService.js";
+
 const Context = React.createContext(null);
 
 const ProviderWrapper = (props) => {
-	const exposedValue = {};
+	const addParticipant = (role, email, password) => {
+		//TO DO traitement différent selon rôle?
+		//TO DO Bcrypt
+		const payload = {
+			email: email,
+			password: password,
+		};
+		participantService
+			.register(payload)
+			.then((response) => console.log(response));
+		// TO DO gestion erreurs -> handler middleware?
+	};
+
+	const login = (email, password) => {
+		//TO DO Bcrypt
+		const payload = {
+			email: email,
+			password: password,
+		};
+		participantService.login(payload).then((response) => console.log(response));
+		// TO DO gestion erreurs -> handler middleware?
+	};
+
+	const exposedValue = { addParticipant, login };
 	return (
 		<Context.Provider value={exposedValue}>{props.children}</Context.Provider>
 	);
