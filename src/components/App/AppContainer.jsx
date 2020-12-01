8fb962c93a43ca0178a8a15c167759b5095f207f
sadra edrisaraki <sadraedrisaraki@gmail.com>
2020-12-01 13:47:29 +0100

    env config

--- src/components/App/AppContainer.jsx
+++ src/components/App/AppContainer.jsx
@@ -3,6 +3,7 @@ import { ProviderWrapper as QRCodeProvider } from "../../contexts/QRCodeContext"
 import App from "./App";
 
 const AppContainer = () => {
+	console.log(process.env.REACT_APP_API_ENDPOINT)
 	return (
 		<QRCodeProvider>
 			<App />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
 	return (
 		<div>
 			<nav className="navbar navbar-dark bg-dark">
-				<span className="navbar-brand mb-0 h1">BlockCovid</span>
+				<span className="navbar-brand mb-0 h1">BlockCovid {process.env.REACT_APP_API_ENDPOINT}</span>
 			</nav>
 		</div>
 	);
