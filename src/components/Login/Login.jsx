957044e045cf6334137a0239cb62bb3a59672c40
Adrien <adrien_lemoine@hotmail.com>
2020-12-01 14:22:17 +0100

    register & login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,49 @@
+import React, { useContext, useState } from "react";
+
+import QRCodeContext from "../../contexts/QRCodeContext";
+
+const Login = () => {
+	const [newEmail, setNewEmail] = useState("");
+	const [newPassword, setNewPassword] = useState("");
+	const { login } = useContext(QRCodeContext);
+
+	const handleLogin = (event) => {
+		event.preventDefault();
+		login(newEmail, newPassword);
+		setNewEmail("");
+		setNewPassword("");
+	};
+
+	const handleEmailChange = (event) => {
+		event.preventDefault();
+		setNewEmail(event.target.value);
+	};
+
+	const handlePasswordChange = (event) => {
+		event.preventDefault();
+		setNewPassword(event.target.value);
+	};
+
+	return (
+		<div>
+			<h1>Se connecter</h1>
+			<form onSubmit={handleLogin}>
+				<div>
+					Email
+					<input type="text" value={newEmail} onChange={handleEmailChange} />
+				</div>
+				<div>
+					Password
+					<input
+						type="text"
+						value={newPassword}
+						onChange={handlePasswordChange}
+					/>
+				</div>
+					<button type="submit">Se connecter</button>
+			</form>
+		</div>
+	);
+};
+
+export default Login;
