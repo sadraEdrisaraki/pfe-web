286169d04c1862197af220971d10923d8a9e54c5
Adrien <adrien_lemoine@hotmail.com>
2020-12-01 11:04:46 +0100

    navbar test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+import Footer from "components/Footer/Footer";
 import Navbar from "components/Navbar/Navbar";
 
 const App = () => {
 	return (
 		<div>
 			<Navbar></Navbar>
+			<Footer></Footer>
 		</div>
 	)
 }
