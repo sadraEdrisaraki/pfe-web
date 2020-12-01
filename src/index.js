1c6394b7a2a1f55c748831c7fce5f242a2da9ceb
sadraEdrisaraki <64404957+sadraEdrisaraki@users.noreply.github.com>
2020-12-01 15:46:29 +0100

    test commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppContainer from './components/App/App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AppContainer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
