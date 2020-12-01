330b247279d41f08f8325098cc23b278280d5936
Adrien <adrien_lemoine@hotmail.com>
2020-12-01 15:39:15 +0100

    services + container

---
+++ src/components/App/AppContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ProviderWrapper as QRCodeProvider } from "../../contexts/QRCodeContext";
+import App from "./App";
+
+const AppContainer = () => {
+	console.log(process.env.REACT_APP_API_ENDPOINT)
+	return (
+		<QRCodeProvider>
+			<App />
+		</QRCodeProvider>
+	);
+};
+
+export default AppContainer;
