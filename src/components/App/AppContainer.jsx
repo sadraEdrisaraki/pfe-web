fdb9eeee998d7d3d806f39bdee480ae1ac66cc6a
sadra edrisaraki <sadraedrisaraki@gmail.com>
2020-12-01 11:02:03 +0100

    adding json reader

--- src/components/App/AppContainer.jsx
+++ src/components/App/AppContainer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import * as config from "./../../config/appsettings.json"
 
 import App from "./App";
+console.log(config.test)
 
 const AppContainer = () => {
 	return <App />;
