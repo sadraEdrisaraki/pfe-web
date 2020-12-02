5ac91dc80da26c63a9df220d025dd739bd8d36f4
sadraEdrisaraki <64404957+sadraEdrisaraki@users.noreply.github.com>
2020-12-02 15:35:22 +0100

    Update AppContainer.jsx

--- src/components/App/AppContainer.jsx
+++ src/components/App/AppContainer.jsx
@@ -3,12 +3,7 @@ import { ProviderWrapper as QRCodeProvider } from "../../contexts/QRCodeContext"
 import App from "./App";
 
 const AppContainer = () => {
-<<<<<<< HEAD
 	console.log(process.env.REACT_APP_API_ENDPOINT)
-	
-=======
-	//console.log(process.env.REACT_APP_API_ENDPOINT)
->>>>>>> 0037aad638122cf16f88f8a8db41c90caa2c9c5d
 	return (
 		<QRCodeProvider>
 			<App />
