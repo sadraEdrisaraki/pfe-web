36dc72bbc8e58b15a34efeac5bd9449450ed6642
Adrien <adrien_lemoine@hotmail.com>
2020-12-03 10:28:30 +0100

    setup proxy modified

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
 	app.use(
-		"/api/citizens",
+		"api/citizens",
 		createProxyMiddleware({
 			target: process.env.REACT_APP_API_ENDPOINT,
 			changeOrigin: true,
