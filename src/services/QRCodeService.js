a4143f4ca0cc02c87051cff76222125fc78cf308
Adrien <adrien_lemoine@hotmail.com>
2020-12-03 10:50:04 +0100

    without proxy

--- src/services/QRCodeService.js
+++ src/services/QRCodeService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const baseUrl = "https://pfe-web-dev.azurewebsites.net/api/citizens";
-
+//const baseUrl = "https://pfe-web-dev.azurewebsites.net/api/citizens";
+const baseUrl=process.env.REACT_APP_API_ENDPOINT+'api/citizens'
 const getAll = (newObject) => {
 	const request = axios.get(baseUrl);
 	return request.then((response) => response.data);
