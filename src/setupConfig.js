d12e2d94aa1e110478a7fe1d0014a55cb46f7bce
Adrien <adrien_lemoine@hotmail.com>
2020-12-03 11:58:44 +0100

    test hostname

--- src/setupConfig.js
+++ src/setupConfig.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
 	app.use(
-		"/api/citizens",
+		"/api/*",
 		createProxyMiddleware({
 			target: "https://pfe-backend-dev.azurewebsites.net",
             changeOrigin: true,
