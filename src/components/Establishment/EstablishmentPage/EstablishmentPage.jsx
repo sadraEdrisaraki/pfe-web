fb36e50d2fe2f3c109bf77ce7c902e30706e538a
sadraEdrisaraki <64404957+sadraEdrisaraki@users.noreply.github.com>
2020-12-04 11:13:45 +0100

    set error message comp

--- src/components/Establishment/EstablishmentPage/EstablishmentPage.jsx
+++ src/components/Establishment/EstablishmentPage/EstablishmentPage.jsx
@@ -1,13 +1,16 @@
 import React from "react"
 import EstablishmentMenu from "components/Establishment/EstablishmentMenu/EstablishmentMenu"
 import "./style.css"
+import AlertMessage from "components/SharedComponents/AlertMessage/AlertMessage"
 
 const EstablishmentPage = () => {
 
     return(
         <div className="establishment-page">
             <EstablishmentMenu/>
+            <AlertMessage type="error" message="pls try again"/>
         </div>
+        
     )
 
 }
