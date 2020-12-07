60ec39de92ec53fccb883936e6450f0bde966425
Adrien <adrien_lemoine@hotmail.com>
2020-12-07 10:25:37 +0100

    select language components & trauductions

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,9 @@
-import React,{useContext,useState} from "react";
+import React from "react";
 import QRCodeContext from "contexts/QRCodeContext";
 
 const Navbar = () => {
-	const { language,setLanguage } = useContext(QRCodeContext);
-
 	const handleLogout = () => {
 		localStorage.setItem("token", null);
-	}
-
-	const handleLanguageChange = (event) => {
-		event.preventDefault();
-		setLanguage(event.target.value);
 	}
 
 	return (
@@ -18,11 +11,6 @@ const Navbar = () => {
 			<nav className="navbar navbar-dark bg-dark">
 				<span className="navbar-brand mb-0 h1">BlockCovid {process.env.REACT_APP_API_ENDPOINT}</span>
 				{ localStorage.getItem("datakey") === null ? <button type="button" className="btn btn-outline-warning" onClick={handleLogout}>Deconnexion</button> : ""}
-				<select value={language} onChange={handleLanguageChange}>
-			<option value="fr">Français</option>
-			<option value="en">Anglais</option>
-			<option value="nl">Néerlandais</option>
-		</select>
 			</nav>
 		</div>
 	);
---
+++ src/components/SharedComponents/SelectLanguage.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import { FormattedMessage } from "react-intl";
+
+import QRCodeContext from "contexts/QRCodeContext";
+
+const SelectLanguage = () => {
+	const { language, setLanguage } = useContext(QRCodeContext);
+
+	const handleLanguageChange = (event) => {
+		event.preventDefault();
+		setLanguage(event.target.value);
+	};
+	return (
+		<select value={language} onChange={handleLanguageChange}>
+			<option value="fr">
+				{" "}
+				<FormattedMessage id="languageOptionFrenchLabel" />
+			</option>
+			<option value="en">
+				<FormattedMessage id="languageOptionEnglishLabel" />
+			</option>
+			<option value="nl">
+				<FormattedMessage id="languageOptionDutchLabel" />
+			</option>
+		</select>
+	);
+};
+
+export default SelectLanguage;
